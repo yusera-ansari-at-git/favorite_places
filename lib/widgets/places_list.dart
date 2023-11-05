2fc3a1210857a3a9a587ce6696f664d64229dc18
yusera-ansari-at-git <142092189+yusera-ansari-at-git@users.noreply.github.com>
2023-11-05 19:30:55 +0530

    239. Adding a "Place Details" Screen (Challenge Solution 6/6)

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -1,4 +1,5 @@
 import 'package:favorite_places/models/place.dart';
+import 'package:favorite_places/screens/places_detail.dart';
 import 'package:flutter/material.dart';
 
 class PlacesList extends StatelessWidget {
@@ -9,15 +10,27 @@ class PlacesList extends StatelessWidget {
   Widget build(BuildContext context) {
     if (places.isEmpty) {
       return Center(
-        child: Text('No places added yet',
-            style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                  color: Theme.of(context).colorScheme.onBackground,
-                )),
+        child: Text(
+          'No places added yet',
+          style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
       );
     }
     return ListView.builder(
       itemBuilder: (ctx, indx) {
         return ListTile(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (ctx) {
+                  return PlaceDetailScreen(place: places[indx]);
+                },
+              ),
+            );
+          },
           title: Text(
             places[indx].title,
             style: Theme.of(context).textTheme.titleMedium!.copyWith(
